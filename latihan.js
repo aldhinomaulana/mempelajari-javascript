d6df727ab4761fbc6d5e62c3d7195ad64305aae2
aldhinomaulana <aldhinomaulana06gmail.com>
2021-06-22 10:49:12 +0700

    menambahkan tipe data doolean

--- latihan.js
+++ latihan.js
@@ -1,7 +1,14 @@
 //javasript
 document.writeln("Hello World")
+document.writeln("</br>")
 
 //Tipe data number
 document.writeln( 100 );
 document.writeln("</br>");
 document.writeln( 100.123 );
+document.writeln("</br>")
+
+//Tipe data Boolean 
+document.writeln(true); //benar
+document.writeln("</br>");
+document.writeln(false); //salah
